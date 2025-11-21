0d2fcd05f56940039bd78eab1d89c8040d407802
Bruno Lima da Silva <ozamodas123@gmail.com>
2025-11-21 09:49:06 -0300

    criando funcoes do semaforo em .h

--- sync_lib.h
+++ sync_lib.h
@@ -9,10 +9,18 @@ typedef struct{
     pthread_mutex_t trava;
 } sync_mutex_t;
 //funcoes do mutex
-void mutex_init(sync_mutex_t *mutex);
-void mutex_lock(sync_mutex_t *mutex);
-void mutex_unlock(sync_mutex_t *mutex);
-void mutex_destroy(sync_mutex_t *mutex);
+void sync_mutex_init(sync_mutex_t *mutex);
+void sync_mutex_lock(sync_mutex_t *mutex);
+void sync_mutex_unlock(sync_mutex_t *mutex);
+void sync_mutex_destroy(sync_mutex_t *mutex);
 
 //struct do semaforo
+typedef struct{
+    sem_t sem;
+}sync_sem_t;
+
+void sync_sem_init(*sync_sem_t semaforo, int valor);
+void sync_sem_wait(*sync_sem_t semaforo);
+void sync_sem_post(*sync_sem_t semaforo);
+void sync_sem_destroy(*sync_sem_t semaforo);
 #endif
